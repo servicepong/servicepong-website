96913f64873262fc83cca256c1b8f2bf3c14243d
Tobias Lindenberg <tobias@project-tampah.com>
2022-07-30 12:01:37 +0200

    faq json ld

--- src/components/pricing/Faq/Faq.tsx
+++ src/components/pricing/Faq/Faq.tsx
@@ -1,3 +1,4 @@
+import { FAQPageJsonLd } from 'next-seo';
 import {
   Accordion,
   AccordionButton,
@@ -8,30 +9,49 @@ import {
   Heading,
 } from '@chakra-ui/react';
 import { useAllFaqQuery } from 'apollo/generated/types';
-import { RichText } from 'prismic-reactjs';
+import { RichText, RichTextBlock } from 'prismic-reactjs';
 
 const Faq = () => {
   // apollo
   const { data: faq } = useAllFaqQuery({ variables: { name: 'Pricing' } });
 
+  const schemaFaqItems = () => {
+    const faqItems: {
+      questionName: string | null | undefined;
+      acceptedAnswerText: RichTextBlock[];
+    }[] = [];
+    if (faq?.allFaqs.edges && faq.allFaqs.edges[0]) {
+      faq.allFaqs.edges[0].node.items?.map((faqItem) => {
+        faqItems.push({
+          questionName: faqItem.question,
+          acceptedAnswerText: faqItem.answer[0].text,
+        });
+      });
+    }
+
+    return faqItems;
+  };
+
   return (
     <>
-      <Heading as="h1" mb={10} size="lg" textAlign="center">
+      <Heading as="h2" mb={10} size="lg" textAlign="center">
         Frequently asked questions
       </Heading>
+      <FAQPageJsonLd mainEntity={schemaFaqItems()} />
+
       <Accordion mt={5} mb={10}>
         {faq?.allFaqs?.edges &&
           faq.allFaqs?.edges[0] &&
           faq.allFaqs?.edges[0].node.items?.map((faqItem) => (
             <AccordionItem key={faqItem?.question}>
-              <h2>
+              <h3>
                 <AccordionButton>
                   <Box flex="1" textAlign="left">
                     <strong>{faqItem?.question}</strong>
                   </Box>
                   <AccordionIcon />
                 </AccordionButton>
-              </h2>
+              </h3>
               <AccordionPanel pb={4}>
                 {RichText.render(faqItem.answer)}
               </AccordionPanel>
